fb46b498a468ad66e8c8073b34ce39137ef8b430
jsaurabh334 <jsaurabh334@gmail.com>
2025-07-07 19:11:47 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/Login';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
+import BlogList from './components/BlogList';
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+               <Route path='/blog' element={<ProtectedRoute><BlogList/></ProtectedRoute>} />
             </Routes>
         </Router>
     );
---
+++ frontend/src/components/BlogList.jsx
@@ -0,0 +1,11 @@
+
+
+const BlogList = () => {
+  return (
+    <div>
+      <h1>create a blog</h1>
+    </div>
+  )
+}
+
+export default BlogList
